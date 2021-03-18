6a4c83cae9c5788c6ee74150e8e7cfd8d1f006c0
SayaliTanawade <tanawade.sayali@gmail.com>
2021-03-18 14:04:14 -0700

    [CITE-85] Updated parameter name in interface as wel

--- spring-social-zotero/src/main/java/org/springframework/social/zotero/api/GroupsOperations.java
+++ spring-social-zotero/src/main/java/org/springframework/social/zotero/api/GroupsOperations.java
@@ -38,5 +38,5 @@ public interface GroupsOperations {
 
     DeletedElements getDeletedElements(String groupId, long version);
 
-    void deleteMultipleItems(String groupId, String citationKey, Long citationVersion) throws ZoteroConnectionException;
+    void deleteMultipleItems(String groupId, String citationKeys, Long citationVersion) throws ZoteroConnectionException;
 }
